eb94aafbae64f27025837261f31335683e3c67e7
HarrisonFitch <harrisonfitch13@gmail.com>
2018-10-05 11:04:24 -0700

    Test File
    Current test file as of 10/5/18

--- test.c
+++ test.c
@@ -1,9 +1,9 @@
 int main() {
     int i = 1 + 2;
-    k = 5 + i;
-    if(k == 0){
+    int k;
+    k = k + 5;
+    if(k > 0)
         k = i;
-    }
     for(int i = 0; i < 3; i = i + 1){
         k = k + 1;
     }
